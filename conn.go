b392ea0299971870d28a465c4d957f2e70e8ced1
stealthycoin <john.w.carlyle@gmail.com>
2015-08-21 11:07:25 -0700

    Allow very big messages by default

--- conn.go
+++ conn.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"math"
 	"sync"
 	"log"
 )
@@ -16,7 +17,7 @@ var (
 	writeWait = 10 * time.Second
 	pongWait = 60 * time.Second
 	pingPeriod = (pongWait * 9) / 10
-	maxMessageSize int64 = 512
+	maxMessageSize int64 = math.MaxInt64
 )
 
 
