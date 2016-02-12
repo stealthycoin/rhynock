35d8bbb4e917709b70555c9f6cd3057198765531
stealthycoin <john.w.carlyle@gmail.com>
2016-02-11 18:27:54 -0800

    Minor race condition on ping time setting

--- conn.go
+++ conn.go
@@ -229,7 +229,9 @@ func (c *Conn) read_write() {
 
 		case <- ticker.C:
 			// Record timestamp of ping
+			c.pingLock.Lock()
 			c.lastping = time.Now().UnixNano()
+			c.pingLock.Unlock()
 
 			// Ping ticker went off. We need to ping to check for connectivity.
 			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
